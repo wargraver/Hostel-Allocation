83abf74261f8c443d2601456c93cf909d369594c
Utkarsh <utkarshkrsingh2091@gmail.com>
2020-11-28 16:35:03 +0530

    https server

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express=require('express')
 const path=require('path')
+const https=require('https')
+const fs=require('fs')
 const app=express()
+const morgan=require('morgan')
 const {route}=require('./db/studentroutes.js')
 const route2=require('./db/room.js').route
+//app.use(morgan('combined'))
+const pvtkey=fs.readFileSync('server.key')
+const cert=fs.readFileSync('server.cert')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(route)
@@ -12,6 +18,6 @@ const port=process.env.PORT || 3000
 app.get('/',(req,res)=>{
     res.status(200).send("hello")
 })
-app.listen(port,()=>{
+https.createServer({key:pvtkey,cert:cert},app).listen(port,()=>{
     console.log("server started on port 3000")
 })

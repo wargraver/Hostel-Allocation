d8edbf694e88eb0bae6ebff07c68f9d4c288e4fa
Utkarsh <utkarshkrsingh2091@gmail.com>
2020-11-28 18:49:54 +0530

    2

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const morgan=require('morgan')
 const {route}=require('./db/studentroutes.js')
 const route2=require('./db/room.js').route
 //app.use(morgan('combined'))
-const pvtkey=fs.readFileSync('server.key')
-const cert=fs.readFileSync('server.cert')
+/*const pvtkey=fs.readFileSync('server.key')
+const cert=fs.readFileSync('server.cert')*/
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(route)
@@ -18,6 +18,6 @@ const port=process.env.PORT || 3000
 app.get('/',(req,res)=>{
     res.status(200).send("hello")
 })
-https.createServer({key:pvtkey,cert:cert},app).listen(port,()=>{
+app.listen(port,()=>{
     console.log("server started on port 3000")
 })
